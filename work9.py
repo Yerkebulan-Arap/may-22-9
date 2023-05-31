3ca0075ab30ade0d71bd3081eba64f37441fa70d
Yerkebulan <134504390+Yerkebulan-Arap@users.noreply.github.com>
2023-05-31 10:20:08 +0600

    Add files via upload

---
+++ work9.py
@@ -0,0 +1,4 @@
+user_inputN = int(input("Введите длину:"))
+user_inputC1 = input("Введите 1-е символ:")
+user_inputC2 = input("Введите 2-е символ:")
+print((user_inputC1+user_inputC2)*int(user_inputN/2))
